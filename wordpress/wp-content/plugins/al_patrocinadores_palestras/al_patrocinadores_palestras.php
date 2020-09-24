9b71d16c769cc90ffb513161749db53a22e95773
farleybsd <farley.t.i@hotmail.com>
2020-09-24 07:34:54 -0300

    Definindo o primeiro plugin

---
+++ wordpress/wp-content/plugins/al_patrocinadores_palestras/al_patrocinadores_palestras.php
@@ -0,0 +1,20 @@
+<?php
+/*
+
+* Plugin Name: Patrocinadores Alura
+* Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:      Selecionar patrocinadores da palestra da Alura
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Farley Rufino
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+*/
+
+/* Testando a chamada do plugin */
+
+//error_log("Estou Dentro do plugin dos patrocinadores");
