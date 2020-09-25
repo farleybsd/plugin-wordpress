926cd9c32f05ce7791cf38e4b9cac01e8fd7c859
farleybsd <farley.t.i@hotmail.com>
2020-09-24 21:33:48 -0300

    Página de configuração

--- wordpress/wp-content/plugins/al_local_dia_palestra/includes/al_local_dia_palestra_settings.php
+++ wordpress/wp-content/plugins/al_local_dia_palestra/includes/al_local_dia_palestra_settings.php
@@ -8,7 +8,7 @@ function al_local_dia_palestra_menu()
         'Local Palestra',
         'Local Palestra',
         'manage_options',
-        'local-palestra',
+        'local-palestra', // slug do menu 
         'al_local_dia_palestra_menu_pagina',
         'dashicons-location-alt',
         -1
@@ -20,4 +20,38 @@ function al_local_dia_palestra_menu()
 function al_local_dia_palestra_menu_pagina()
 {
 
+
+    ?>
+    <div>
+        <h1>
+            Local Palestras
+        </h1>
+        <form method="post" action="options.php">
+            <?php
+
+                do_settings_sections('local-palestra');
+
+            ?>
+        </form>
+    </div>
+    <?php
+
+
+}
+
+add_action('admin_menu','al_local_dia_palestra_secao');
+
+function al_local_dia_palestra_secao()
+{
+    add_settings_section(
+        'al_local_dia_palestra_secao',
+        'configuracoes do local da palestra',
+        'al_local_dia_palestra_campos_secao_detalhes',
+        'local-palestra' //slug do menu
+    );
+}
+
+function al_local_dia_palestra_campos_secao_detalhes()
+{
+    
 }
