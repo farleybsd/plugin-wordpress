a69a9f708ede6f51e327a9116e650b21e2d107a4
farleybsd <farley.t.i@hotmail.com>
2020-09-24 22:11:49 -0300

    Finalizando a página de configuração

--- wordpress/wp-content/plugins/al_local_dia_palestra/includes/al_local_dia_palestra_settings.php
+++ wordpress/wp-content/plugins/al_local_dia_palestra/includes/al_local_dia_palestra_settings.php
@@ -31,6 +31,7 @@ function al_local_dia_palestra_menu_pagina()
 
                 do_settings_sections('local-palestra');
                 settings_fields('al_local_dia_palestra_settings');
+                submit_button();
             ?>
         </form>
     </div>
@@ -43,6 +44,7 @@ add_action('admin_menu','al_local_dia_palestra_secao');
 
 function al_local_dia_palestra_secao()
 {
+    //seção
     add_settings_section(
         'al_local_dia_palestra_secao',
         'configuracoes do local da palestra',
@@ -50,6 +52,7 @@ function al_local_dia_palestra_secao()
         'local-palestra' //slug do menu
     );
 
+    // Endereço
     add_settings_field(
         'al_local-palestra_endereco',
         'Endereço',
@@ -62,6 +65,35 @@ function al_local_dia_palestra_secao()
         'al_local_dia_palestra_settings',
         'al_local-palestra_endereco' // campo
     );
+
+    // cidade
+    add_settings_field(
+        'al_local-palestra_cidade',
+        'Cidade',
+        'al_local_dia_palestra_cidade',
+        'local-palestra', // slug
+        'al_local_dia_palestra_secao' //secao
+    );
+
+    register_setting(
+        'al_local_dia_palestra_settings',
+        'al_local-palestra_cidade' // campo
+    );
+
+    //data 
+
+    add_settings_field(
+        'al_local-palestra_data',
+        'Data',
+        'al_local_dia_palestra_data',
+        'local-palestra', // slug
+        'al_local_dia_palestra_secao' //secao
+    );
+
+    register_setting(
+        'al_local_dia_palestra_settings',
+        'al_local-palestra_data' // campo
+    );
 }
 
 function al_local_dia_palestra_campos_secao_detalhes()
@@ -73,12 +105,35 @@ function al_local_dia_palestra_campos_secao_detalhes()
     <?php
 }
 
+// Função Call Back Endereco
 function al_local_dia_palestra_endereco()
 {
     ?>
 
     <input type="text" id="al_local-palestra_endereco"
             name="al_local-palestra_endereco" require>    
+
+    <?php
+}
+
+// Funcao Call Back Cidade
+function al_local_dia_palestra_cidade()
+{
+    ?>
+
+    <input type="text" id="al_local-palestra_cidade"
+            name="al_local-palestra_cidade" require>    
+
+    <?php
+}
+
+// Funçao Call Back Data
+function al_local_dia_palestra_data()
+{
+    ?>
+
+    <input type="date" id="al_local-palestra_data"
+            name="al_local-palestra_data" require>    
 
     <?php
 }
